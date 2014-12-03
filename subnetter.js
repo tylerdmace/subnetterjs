7c3614446a4bbd4cb84b20e5fbe316eae87ca13e
Tyler Mace <tylerdmace@gmail.com>
2014-12-02 23:14:59 -0700

    added new function for converting dotted decimal to straight decimal

--- subnetter.js
+++ subnetter.js
@@ -17,6 +17,20 @@ var Subnetter = (function(obj) {
     return convertedAddress;
   }
 
+  obj.addressToDecimal = function(address) {
+    var address = obj.addressToBinary(address).join('');
+    var bitPlaceValues = [2147483648, 1073741824, 536870912, 268435456, 134217728, 67108864, 33554432, 16777216, 8388608, 4194304, 2097152, 1048576, 524288, 262144, 131072, 65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1];
+    var bitValue = 0;
+
+    for(var bit = 0; bit < 32; bit++) {
+      if(address.charAt(bit) === '1') {
+        bitValue = bitValue + bitPlaceValues[bit];
+      }
+    }
+
+    return bitValue;
+  }
+
   obj.andOctets = function(octetOne, octetTwo){
     var convertedOctet = [];
 
